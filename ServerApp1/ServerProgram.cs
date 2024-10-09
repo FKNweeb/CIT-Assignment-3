97d2b86a70de5e3b22c120b9ab5856e0b0f08181
ronysharif <89507734+sharifkea@users.noreply.github.com>
2024-10-10 01:30:58 +0200

    # test_3
    3 test done

--- ServerApp1/ServerProgram.cs
+++ ServerApp1/ServerProgram.cs
@@ -17,5 +17,41 @@ while (true)
     var client = server.AcceptTcpClient();
     Console.WriteLine("Client connected!!!");
 
-   
+    try
+    {
+        var stream = client.GetStream();
+
+        var buffer = new byte[1024];
+
+        var readCount = stream.Read(buffer);
+        var msg = Encoding.UTF8.GetString(buffer, 0, readCount);
+        Console.WriteLine(msg);
+        var response = new Response();
+        if (msg == "{}")
+        {
+            response.Status = "missing method";
+        }
+
+        else
+
+        {
+            var request = JsonSerializer.Deserialize<Request>(msg, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            if (request.Method == "xxxx")
+            {
+                response.Status = "illegal method";
+            }
+        }
+
+        var json = JsonSerializer.Serialize(response, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+        Console.WriteLine(json);
+
+        buffer = Encoding.UTF8.GetBytes(json);
+        stream.Write(buffer);
+
+
+
+
+
+    }
+    catch { }
 }
